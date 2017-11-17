e792558ce9822e8c2109765893c634b925216867
katapo <kata.pochec@gmail.com>
2017-11-17 22:23:09 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
-$(function(){
+$(function() {
 
 
-	var $span = $('span');
-	$span.each(function(index, element){
-		if (index % 2 == 0) $(element).css('color', 'red');
-	})
+    var $span = $('span');
+    $span.each(function(index, element) {
+        if (index % 2 == 0) $(element).css('color', 'red');
+    })
 
-	var paragraphs = $('p');
-	paragraphs.each(function(index, element) {
+    var paragraphs = $('p');
+    paragraphs.each(function(index, element) {
 
-		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-		$(element).append(button)
+        var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+        $(element).append(button)
 
-	})
+    })
 
-	$('button').click(function(){
-		alert($(this).attr('data-tmp'));
-	})
+    $('button').click(function() {
+        alert($(this).attr('data-tmp'));
+    })
 
 });
